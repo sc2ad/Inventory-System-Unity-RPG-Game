27b1aacaedb88a4fd990c14052f3b890743b411a
BattleDoog <austin.soohoo@gmail.com>
2015-09-25 12:59:15 -0700

    Create ItemClassProperties.js

---
+++ ItemClassProperties.js
@@ -0,0 +1,18 @@
+
+public var image : GameObject;
+public var name = "";
+public var val : int;
+public var damage : int;
+public var durability : int;
+enum type{equippable_hand=0, equippable_body=1, equippable_accessory=2, junk=3, quest=4};
+public var typerz : type;
+
+
+
+// Use this for initialization
+function Start () {
+}
+
+// Update is called once per frame
+function Update () {
+}
